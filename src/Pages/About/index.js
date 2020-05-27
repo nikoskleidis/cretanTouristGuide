4ce8952a49e9cc94128705ff73714c65a091447a
unknown <gior.gos21@hotmail.com>
2020-05-27 14:56:12 +0300

    Mia prwth allagh sto about page!!!

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React from "react";
+//import photo from "./photo.png";//
 import "./styles.css"
+//console.log(photo);//
 
-function About() {
+
+function About_page() {
   return (
-    <div className={"About_wrapper"}>
-      <h2>About</h2>
+    <div className={"About_page"}>
+      <div class="p-gnwrimia">
+        <p>Ονομάζομαι Βογιατζής Γεώργιος και η πτυχιακή μου εργασία αφορά  έναν πλήρη τουριστικό οδηγό της Κρήτης</p>
+      </div>
+      <div class="p-ergaleia">
+        <p>Τα εργαλεια που θα χρησιμοποιήσω για την διεκπεραίωσή της είναι τα εξής:</p>
+        </div>
+      <ul>
+        <li>Vs Code(Visual Studio Code)</li>
+        <li>React</li>
+        <li>React-router</li>
+      </ul>
+      
     </div>
   )
+  
 }
 
-export default About
+export default About_page
