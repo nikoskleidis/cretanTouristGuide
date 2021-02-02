dab29be785dd3633282dedc843cce7b8605158b4
giorvog <gior.gos21@hotmail.com>
2021-02-02 16:27:49 +0200

    Updates

---
+++ precache-manifest.f56d7e5d344851683124dc599990ab73.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc66ec77839f86a248039b85b764e79",
+    "url": "/My-Cretantouristguide/index.html"
+  },
+  {
+    "revision": "23ca20b4f1a21155033a",
+    "url": "/My-Cretantouristguide/static/css/main.a812dc08.chunk.css"
+  },
+  {
+    "revision": "53d88a26e62044d37d3a",
+    "url": "/My-Cretantouristguide/static/js/2.aa7c3fcb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/My-Cretantouristguide/static/js/2.aa7c3fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ca20b4f1a21155033a",
+    "url": "/My-Cretantouristguide/static/js/main.1639ef55.chunk.js"
+  },
+  {
+    "revision": "e71a9ae0d4ff2395b791",
+    "url": "/My-Cretantouristguide/static/js/runtime-main.59a35cc6.js"
+  },
+  {
+    "revision": "6ddd03f3658d2873ec90b70440880c93",
+    "url": "/My-Cretantouristguide/static/media/Ag.Nikolaos.6ddd03f3.jpg"
+  },
+  {
+    "revision": "da3d9d72d64633f0967d4ab317017c2c",
+    "url": "/My-Cretantouristguide/static/media/Chania.da3d9d72.jpg"
+  },
+  {
+    "revision": "80d8fc97ef675e20909a8d5ae097ec7a",
+    "url": "/My-Cretantouristguide/static/media/Heraklion.80d8fc97.png"
+  },
+  {
+    "revision": "2bb6673b2a6fad59e2f295299a36f97e",
+    "url": "/My-Cretantouristguide/static/media/me.2bb6673b.png"
+  },
+  {
+    "revision": "2552297b7b75d9f2e7f5a5ffb932b16d",
+    "url": "/My-Cretantouristguide/static/media/rethymno.2552297b.png"
+  }
+]);
